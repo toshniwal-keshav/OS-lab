f953b1bc5d59183f3876864b94902ce72a9a2e06
toshniwal-keshav <keshavtoshniwal.cs24@bmsce.ac.in>
2026-06-10 15:36:04 +0530

    Refactor EDF scheduling implementation

--- 3b_EDF.c
+++ 3b_EDF.c
@@ -46,7 +46,7 @@ void EDF_Preemptive(struct Process t[], int n) {
         for (int i = 0; i < n; i++) {
             if (time == t[i].next_at) {
                 if(t[i].rt > 0) 
-                     printf("Deadline Miss by Process %d at time %d\n",t[idx].id, finish);
+                     printf("Deadline Miss by Process %d at time %d\n",t[idx].id, time);
                 t[i].rt = t[i].bt;
                 t[i].arrival_time = time;
                 t[i].deadline = time + t[i].period;
