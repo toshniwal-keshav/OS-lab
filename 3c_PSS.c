93cc83b81eec6d8a38edf04f9b1fbd31987fcb7f
toshniwal-keshav <keshavtoshniwal.cs24@bmsce.ac.in>
2026-06-10 15:50:01 +0530

    Fix formatting and improve code readability

--- 3c_PSS.c
+++ 3c_PSS.c
@@ -19,7 +19,7 @@ int totalTime(int n){
     return tot;
 }
 void PSS(int n){
-    int t=0,done=0,last=-1,q;
+    int t=0,done=0,last=0,q;
     int tot = totalTime(n);
     float awt=0.0, atat=0.0;
     int gantt[tot];
